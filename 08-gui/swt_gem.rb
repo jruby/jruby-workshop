945f6a3435912138482369c53f58d284a6081a68
Hiro Asari <asari.ruby@gmail.com>
2013-02-17 13:12:20 -0500

    SWT example from Dan Lucraft's swt gem

---
+++ 08-gui/swt_gem.rb
@@ -0,0 +1,53 @@
+require 'swt'
+
+class ButtonExample
+
+  def initialize
+    # A Display is the connection between SWT and the native GUI.
+    display = Swt::Widgets::Display.get_current
+  
+    # A Shell is a window in SWT parlance.
+    @shell = Swt::Widgets::Shell.new
+    
+    # Set the window title bar text
+    @shell.text = "Button Example"
+    
+    # A Shell must have a layout. FillLayout is the simplest.
+    layout = Swt::Layout::FillLayout.new
+    @shell.setLayout(layout)
+
+    # Create a button widget
+    button = Swt::Widgets::Button.new(@shell, Swt::SWT::PUSH)
+    button.set_text("Click Me")
+    
+    # Add a button click listener
+    button.add_selection_listener do
+      puts "You clicked the button."
+    end
+    
+    # This lays out the widgets in the Shell
+    @shell.pack
+    
+    # And this displays the Shell
+    @shell.open
+  end
+  
+  # This is the main gui event loop
+  def start
+    display = Swt::Widgets::Display.get_current
+    
+    # until the window (the Shell) has been closed
+    while !@shell.isDisposed
+    
+      # check for and dispatch new gui events
+      display.sleep unless display.read_and_dispatch
+    end
+
+    display.dispose
+  end
+end
+
+Swt::Widgets::Display.set_app_name "Button Example"
+ 
+app = ButtonExample.new
+app.start
